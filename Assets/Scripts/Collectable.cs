0aa243d6aa89da3d84326d9c261dd734d9eb7e18
faddan <43156911+faddan@users.noreply.github.com>
2018-12-11 12:47:20 -0600

    added collectabe with animation, changed player script

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private static int coinCount;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            coinCount++;
+            Debug.Log("Coin count: " + coinCount);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    private bool isPlayerInTrigger;
+
+    // Can't use this because it triggers the door twice
+    // due to player having multiple colliders
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        if (Input.GetButtonDown("Activate"))
+    //        {
+    //            Debug.Log("Player activated door.");
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Activate") && isPlayerInTrigger)
+        {
+            Debug.Log("Player activated door.");
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
